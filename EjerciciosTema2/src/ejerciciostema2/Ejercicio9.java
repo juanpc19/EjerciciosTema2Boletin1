1e73556aa65ef36e8db4ef835432950328ab0099
jperez <jperez@106-20.info2>
2022-10-07 14:04:57 +0200

    9 empezao

---
+++ EjerciciosTema2/src/ejerciciostema2/Ejercicio9.java
@@ -0,0 +1,28 @@
+package ejerciciostema2;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		
+		int jugador1, jugador2, jugador3;
+		String PIEDRA, b="PAPEL", c="TIJERA";
+		
+		Scanner dogma = new Scanner(System.in);
+		
+		System.out.print("Escriba PIEDRA, PAPEL O TIJERA: ");
+		
+		 = dogma.next();
+		
+		System.out.print("Escriba PIEDRA, PAPEL O TIJERA: ");
+		
+		
+		
+		System.out.print("Escriba PIEDRA, PAPEL O TIJERA: ");
+		
+		
+
+	}
+
+}
